6e8a924b5ca3572e212e3b58677a3735bf76975a
rais11kun <rais24ti@mahasiswa.pcr.ac.id>
2025-12-18 22:06:31 +0700

    2

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,25 @@ Route::get('/', function () {
 });
 
 route::get('/index', function(){
-    return view('index');
+    return view('landing/index');
 });
+
+Route::get('/', function () {
+    return view('landing.index');
+})->name('home');
+
+Route::get('/about', function () {
+    return view('landing.about');
+})->name('about');
+
+Route::get('/donation', function () {
+    return view('landing.donation');
+})->name('donation');
+
+Route::get('/event', function () {
+    return view('landing.event');
+})->name('event');
+
+Route::get('/contact', function () {
+    return view('landing.contact');
+})->name('contact');
